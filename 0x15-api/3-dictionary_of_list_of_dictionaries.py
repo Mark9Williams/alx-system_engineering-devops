87dd6ad1ed7c86668939220a59887607e71cc188
Mark9Williams <mawejjemarkwilliam@gmail.com>
2024-07-29 19:28:27 +0300

    dictionary_list_of_dictionaries

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+import json
+import requests
+
+# Fetch all users
+users_url = "https://jsonplaceholder.typicode.com/users"
+users_response = requests.get(users_url)
+users = users_response.json()
+
+# Dictionary to hold data for all employees
+all_tasks = {}
+
+# Fetch all TODOs
+todos_url = "https://jsonplaceholder.typicode.com/todos"
+todos_response = requests.get(todos_url)
+todos = todos_response.json()
+
+# Structure data in the required format
+for user in users:
+    user_id = str(user['id'])
+    username = user['username']
+    
+    # Filter todos for the current user
+    user_todos = [todo for todo in todos if todo['userId'] == user['id']]
+    
+    # Format the data as required
+    all_tasks[user_id] = [
+        {
+            "username": username,
+            "task": todo['title'],
+            "completed": todo['completed']
+        }
+        for todo in user_todos
+    ]
+
+# Write the data to a JSON file
+with open('todo_all_employees.json', mode='w') as jsonfile:
+    json.dump(all_tasks, jsonfile)
