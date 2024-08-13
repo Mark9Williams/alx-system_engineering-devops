23fd80f685afe27de0294ceb61e4098e085d9221
Mark9Williams <mawejjemarkwilliam@gmail.com>
2024-08-13 17:54:55 +0300

    top ten

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import requests
+
+
+def top_ten(subreddit):
+    """Prints the titles of the first 10 hot posts for a given subreddit."""
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    headers = {'User-Agent': 'custom-user-agent'}
+
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            data = response.json()
+            posts = data.get("data", {}).get("children", [])
+            for post in posts:
+                print(post.get("data", {}).get("title"))
+        else:
+            print(None)
+    except requests.RequestException:
+        print(None)
